6f469e76c025a3e6d3e51e19e3915856d89ce754
rena1208 <jajajajam07@gmail.com>
2025-04-28 13:00:14 +0900

    postしたgeojsonの地物と属性数を返すエンドポイントの作成

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 
-from .themes import theme_01, theme_02, theme_03
+from .themes import theme_01, theme_02, theme_03, theme_04
 
 app = FastAPI()
 
 app.include_router(theme_01.router, prefix="/themes")
 app.include_router(theme_02.router, prefix="/themes")
 app.include_router(theme_03.router, prefix="/themes")
+app.include_router(theme_04.router, prefix="/themes")
 
 @app.get("/health")
 def health():
